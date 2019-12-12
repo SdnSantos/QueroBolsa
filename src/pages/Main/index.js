b7bd7f3be496d3e54802beecd81456bae906b0f1
Sidney Santos <sidney_ssantos@hotmail.com>
2019-12-11 21:53:53 -0300

    Estrutura do Projeto

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+export default function Main() {
+  return (
+    <Title error>
+      Main
+      <br />
+      <small>sub-Main</small>
+    </Title>
+  );
+}
